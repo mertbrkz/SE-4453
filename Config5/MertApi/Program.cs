0b8b5cafdb26d158e4d198d55b191619cc248446
MertBarakazi <mert.barakazi@rdc.com.tr>
2025-12-26 16:51:10 +0300

    Disable KV to isolate startup error

--- Config5/MertApi/Program.cs
+++ Config5/MertApi/Program.cs
@@ -7,12 +7,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configure KeyVault
-var keyVaultName = builder.Configuration["KeyVaultName"];
-if (!string.IsNullOrEmpty(keyVaultName))
-{
-    var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
-    builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
-}
+// var keyVaultName = builder.Configuration["KeyVaultName"];
+// if (!string.IsNullOrEmpty(keyVaultName))
+// {
+//     var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+//     builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+// }
 
 var app = builder.Build();
 
